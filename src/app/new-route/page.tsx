04ae29abdf96ef00a113486db09d0c290338102b
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-08-25 01:21:47 +0000

    feature(web-tracking): map preview rendering

--- src/app/new-route/page.tsx
+++ src/app/new-route/page.tsx
@@ -1,59 +1,92 @@
-'use client'
+"use client";
 
-import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js"
-import { FormEvent } from "react"
+import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js";
+import { Loader } from "@googlemaps/js-api-loader";
+import { FormEvent, useEffect } from "react";
 
 export function NewRoutePage() {
+  useEffect(() => {
+    (async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+        libraries: ["routes", "geometry"],
+      });
+      await Promise.all([
+        loader.importLibrary("routes"),
+        loader.importLibrary("geometry"),
+      ]);
+      new google.maps.Map(document.getElementById("map") as any, {
+        zoom: 15,
+        // é necessário dar uma coordenada para a renderização prévia
+        center: { lat: -29.6846, lng: -51.1419 },
+      });
+    })();
+  }, []);
+
   async function searchPlaces(event: FormEvent) {
-    event.preventDefault()
-    const source = (document.getElementById("source") as HTMLInputElement).value
-    const destination = (document.getElementById("destination") as HTMLInputElement).value
-    
+    event.preventDefault();
+    const source = (document.getElementById("source") as HTMLInputElement)
+      .value;
+    const destination = (
+      document.getElementById("destination") as HTMLInputElement
+    ).value;
+
     const [sourceResponse, destinationResponse] = await Promise.all([
       fetch(`http://localhost:3000/places?text=${source}`),
       fetch(`http://localhost:3000/places?text=${destination}`),
-    ])
+    ]);
 
-    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] = await Promise.all([
-      sourceResponse.json(),
-      destinationResponse.json(),
-    ])
+    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] =
+      await Promise.all([sourceResponse.json(), destinationResponse.json()]);
 
-    if (sourcePlace.status !== 'OK') {
-      console.error(sourcePlace)
-      alert('Origem não encontrada')
-      return
+    if (sourcePlace.status !== "OK") {
+      console.error(sourcePlace);
+      alert("Origem não encontrada");
+      return;
     }
 
-    if (destinationPlace.status !== 'OK') {
-      console.error(destinationPlace)
-      alert('Destino não encontrado')
-      return
+    if (destinationPlace.status !== "OK") {
+      console.error(destinationPlace);
+      alert("Destino não encontrado");
+      return;
     }
 
-    const placeSourceId = sourcePlace.candidates[0].place_id
-    const placeDestinationId = destinationPlace.candidates[0].place_id
+    const placeSourceId = sourcePlace.candidates[0].place_id;
+    const placeDestinationId = destinationPlace.candidates[0].place_id;
 
     const directionsResponse = await fetch(
       `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`
-    )
-    const directionsData = await directionsResponse.json()
+    );
+    const directionsData = await directionsResponse.json();
   }
 
   return (
-    <div>
-      <h1>Nova rota</h1>
-      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={searchPlaces} >
-        <div>
-          <input id="source" type="text" placeholder="origem" />
-        </div>
-        <div>
-          <input id="destination" type="text" placeholder="destino" />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <div>
+        <h1>Nova rota</h1>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={searchPlaces}
+        >
+          <div>
+            <input id="source" type="text" placeholder="origem" />
           </div>
-      <button type="submit" >Pesquisar</button>
-      </form>
-     </div>
-   )
+          <div>
+            <input id="destination" type="text" placeholder="destino" />
+          </div>
+          <button type="submit">Pesquisar</button>
+        </form>
+      </div>
+      <div id="map" style={{ height: "100%", width: "100%" }}></div>
+    </div>
+  );
 }
 
-export default NewRoutePage
+export default NewRoutePage;
