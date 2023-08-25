6c7ce04c10fcc9c24f1cea5e6e41864e6ab4113a
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-08-25 02:35:14 +0000

    feature(web-tracking):route trace rendered on map when choosing travel

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import type {
-  DirectionsResponseData,
-  FindPlaceFromTextResponseData,
-} from "@googlemaps/google-maps-services-js";
-import { FormEvent, useRef, useState } from "react";
+import { useRef } from "react";
 import { useMap } from "../hooks/useMap";
-import { useSWRConfig } from "swr";
 import useSwr from "swr";
 import { fetcher } from "../utils/http";
 import { Route } from "../utils/model";
@@ -14,9 +9,6 @@ import { Route } from "../utils/model";
 export function DriverPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const map = useMap(mapContainerRef);
-  const [directionsData, setDirectionsData] = useState<
-    DirectionsResponseData & { request: any }
-  >();
 
   const {
     data: routes,
@@ -26,73 +18,24 @@ export function DriverPage() {
     fallbackData: [],
   });
 
-  async function searchPlaces(event: FormEvent) {
-    event.preventDefault();
-    const source = (document.getElementById("source") as HTMLInputElement)
+  async function startRoute() {
+    const routeId = (document.getElementById("route") as HTMLSelectElement)
       .value;
-    const destination = (
-      document.getElementById("destination") as HTMLInputElement
-    ).value;
-
-    const [sourceResponse, destinationResponse] = await Promise.all([
-      fetch(`http://localhost:3000/places?text=${source}`),
-      fetch(`http://localhost:3000/places?text=${destination}`),
-    ]);
-
-    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] =
-      await Promise.all([sourceResponse.json(), destinationResponse.json()]);
-
-    if (sourcePlace.status !== "OK") {
-      console.error(sourcePlace);
-      alert("Origem não encontrada");
-      return;
-    }
-
-    if (destinationPlace.status !== "OK") {
-      console.error(destinationPlace);
-      alert("Destino não encontrado");
-      return;
-    }
-
-    const placeSourceId = sourcePlace.candidates[0].place_id;
-    const placeDestinationId = destinationPlace.candidates[0].place_id;
-
-    const directionsResponse = await fetch(
-      `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`
-    );
-    const directionsData: DirectionsResponseData & { request: any } =
-      await directionsResponse.json();
-    setDirectionsData(directionsData);
+    const response = await fetch(`http://localhost:3000/routes/${routeId}`);
+    const route: Route = await response.json();
     map?.removeAllRoutes();
     await map?.addRouteWithIcons({
-      routeId: "1",
+      routeId: routeId,
       startMarkerOptions: {
-        position: directionsData.routes[0].legs[0].start_location,
+        position: route.directions.routes[0].legs[0].start_location,
       },
       endMarkerOptions: {
-        position: directionsData.routes[0].legs[0].end_location,
+        position: route.directions.routes[0].legs[0].end_location,
       },
       carMarkerOptions: {
-        position: directionsData.routes[0].legs[0].start_location,
+        position: route.directions.routes[0].legs[0].start_location,
       },
     });
-  }
-
-  async function createRoute() {
-    const startAddress = directionsData!.routes[0].legs[0].start_address;
-    const endAddress = directionsData!.routes[0].legs[0].end_address;
-    const response = await fetch("http://localhost:3000/routes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: `${startAddress} - ${endAddress}`,
-        source_id: directionsData!.request.origin.place_id,
-        destination_id: directionsData!.request.destination.place_id,
-      }),
-    });
-    const route = await response.json();
   }
 
   return (
@@ -115,7 +58,9 @@ export function DriverPage() {
               </option>
             ))}
           </select>
-          <button type="submit">Iniciar a viagem</button>
+          <button type="submit" onClick={startRoute}>
+            Iniciar a viagem
+          </button>
         </div>
       </div>
       <div
